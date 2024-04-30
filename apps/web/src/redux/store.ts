2a29f8ce7e4fcaf000e26a14d4b5eca4c180b60f
Sandy Pratama <158142351+A5tama@users.noreply.github.com>
2024-04-30 15:44:26 +0700

    Register (#1)
    * chore: initial setup
    
    * feat: add register

---
+++ apps/web/src/redux/store.ts
@@ -0,0 +1,15 @@
+import { configureStore } from '@reduxjs/toolkit';
+import userReducer from "./slices/userSlice";
+
+export const makeStore = () => {
+  return configureStore({
+    reducer: {
+        user: userReducer,
+    }
+  })
+}
+
+// Infer the type of makeStore
+export type AppStore = ReturnType<typeof makeStore>
+export type RootState = ReturnType<AppStore['getState']>
+export type AppDispatch = AppStore['dispatch']
