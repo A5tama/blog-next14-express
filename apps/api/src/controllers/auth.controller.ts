b7f4879c7ba18a0f0f14dfae003f30702b95d84a
Sandy Pratama <sandypratama25bisnis@gmail.com>
2024-05-02 15:38:01 +0700

    feat: forgot-password

--- apps/api/src/controllers/auth.controller.ts
+++ apps/api/src/controllers/auth.controller.ts
@@ -1,3 +1,4 @@
+import { forgotPasswordService } from "@/service/auth/forgot-password.service";
 import { loginService } from "@/service/auth/login.service";
 import { registerService } from "@/service/auth/register.service";
 import { NextFunction, Request, Response } from "express";
@@ -15,6 +16,14 @@ export class AuthController {
     async loginController( req: Request, res:Response, next: NextFunction ) {
         try {
             const result = await loginService(req.body);
+            return res.status(200).send(result);
+        } catch (error) {
+            next (error);
+        }
+    }
+    async forgotPasswordController( req: Request, res:Response, next: NextFunction ) {
+        try {
+            const result = await forgotPasswordService(req.body.email);
             return res.status(200).send(result);
         } catch (error) {
             next (error);
