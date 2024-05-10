c66beab73aab5c78ba9554df81da714ba9b25def
Sandy Pratama <158142351+A5tama@users.noreply.github.com>
2024-05-10 15:48:18 +0700

    feat: add homepage (#7)

--- apps/web/src/components/BlogCard.tsx
+++ apps/web/src/components/BlogCard.tsx
@@ -1,12 +1,9 @@
-import {
-    Card,
-    CardContent,
-    CardHeader
-} from '@/components/ui/card';
+import { Card, CardContent, CardHeader } from '@/components/ui/card';
 import { format } from 'date-fns';
 import Image from 'next/image';
 import { FC } from 'react';
 import { Badge } from './ui/badge';
+import Link from 'next/link';
 
 interface BlogCardProps {
   title: string;
@@ -15,6 +12,7 @@ interface BlogCardProps {
   author: string;
   imageUrl: string;
   createdAt: Date;
+  blogId: number;
 }
 
 const BlogCard: FC<BlogCardProps> = ({
@@ -24,25 +22,33 @@ const BlogCard: FC<BlogCardProps> = ({
   description,
   imageUrl,
   createdAt,
+  blogId,
 }) => {
   return (
-    <Card>
-      <CardHeader>
-        <div className="relative h-[220px] w-full overflow-hidden rounded-md">
-          <Image src={''} alt="Thumbnail" className="object-cover" fill />
-        </div>
-      </CardHeader>
-      <CardContent>
-        <Badge variant="outline" className="rounded-sm bg-green-100">
-          {category}
-        </Badge>
-        <h2 className="line-clamp-2 text-lg font-semibold">{title}</h2>
-        <p className="text-sm font-light italic">
-          {format(createdAt, 'dd MMMM yyyy')} - {author}
-        </p>
-        <p className="line-clamp-3 ">{description}</p>
-      </CardContent>
-    </Card>
+    <Link href={`/${blogId}`}>
+      <Card>
+        <CardHeader>
+          <div className="relative h-[220px] w-full overflow-hidden rounded-md">
+            <Image
+              src={imageUrl}
+              alt="Thumbnail"
+              className="object-cover"
+              fill
+            />
+          </div>
+        </CardHeader>
+        <CardContent>
+          <Badge variant="outline" className="rounded-sm bg-green-100">
+            {category}
+          </Badge>
+          <h2 className="line-clamp-2 text-lg font-semibold">{title}</h2>
+          <p className="text-sm font-light italic">
+            {format(createdAt, 'dd MMMM yyyy')} - {author}
+          </p>
+          <p className="line-clamp-3 ">{description}</p>
+        </CardContent>
+      </Card>
+    </Link>
   );
 };
 
